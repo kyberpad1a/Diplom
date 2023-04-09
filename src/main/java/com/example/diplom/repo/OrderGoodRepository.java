9719019c739bb901a5655836c49e83da0a5206bc
kyberpad1a <isip_n.s.ignatov@mpt.ru>
2023-04-09 23:00:32 +0300

    Корзина

--- src/main/java/com/example/diplom/repo/OrderGoodRepository.java
+++ src/main/java/com/example/diplom/repo/OrderGoodRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OrderGoodRepository extends JpaRepository<modelOrderGood, Long> {
     modelOrderGood findByGoods_IDGoodAndOrder_User_IDUser(Long IDGood, Long IDUser);
     Collection<modelOrderGood> findAllByOrder_PaymentStatusAndOrder_User_IDUser(boolean flag, Long id);
+
+
 }
--- src/main/java/com/example/diplom/view/user/shoppingCart.java
+++ src/main/java/com/example/diplom/view/user/shoppingCart.java
@@ -8,18 +8,22 @@ import com.example.diplom.repo.UserRepository;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 @PageTitle("Корзина")
 @Route(value = "/cart", layout = userPage.class)
@@ -31,9 +35,36 @@ public class shoppingCart extends VerticalLayout {
     OrderGoodRepository orderGoodRepository;
     Long id;
     public shoppingCart(UserRepository userRepository, OrderGoodRepository orderGoodRepository){
-        grid.addColumn(item -> item.getGoods().getGood_Name()).setHeader("Товар").setWidth("65%");
-        grid.addColumn(modelOrderGood::getGoodQuantity).setHeader("Количество");
+        Label total = new Label();
+        Collection<modelOrderGood> modelOrderGood = orderGoodRepository.findAllByOrder_PaymentStatusAndOrder_User_IDUser(false, id);
+        double price = 0;
+        for (modelOrderGood item:modelOrderGood) {
+
+            price+=item.getGoods().getGood_Price();
+            total.setText(String.valueOf(price));
+        }
+
+        grid.addColumn(item -> item.getGoods().getGood_Name()).setHeader("Товар").setWidth("55%").setFooter(total);
+        grid.addColumn(item -> item.getGoods().getGood_Price()).setHeader("Цена");
+//        grid.addColumn(modelOrderGood::getGoodQuantity).setHeader("Количество");
         Button btnConfirm=new Button("Оформить заказ");
+        grid.addComponentColumn(item->{
+
+
+            HorizontalLayout layout = new HorizontalLayout();
+            IntegerField field = new IntegerField();
+            field.setValue(item.getGoodQuantity());
+            layout.add(field);
+            field.setStep(1);
+            field.setStepButtonsVisible(true);
+            field.addValueChangeListener(integerFieldIntegerComponentValueChangeEvent -> {
+               item.setGoodQuantity(integerFieldIntegerComponentValueChangeEvent.getValue());
+               item.setOrder(item.getOrder());
+               item.setGoods(item.getGoods());
+               orderGoodRepository.save(item);
+            });
+            return layout;
+        }).setHeader("Количество").setTextAlign(ColumnTextAlign.START).setFooter(btnConfirm).setWidth("15%");
         grid.addComponentColumn(item->{
             Button btnDelete =new Button(new Icon(VaadinIcon.CLOSE));
 
@@ -44,7 +75,7 @@ public class shoppingCart extends VerticalLayout {
                 gridRefresh(orderGoodRepository, userRepository);
             });
                     return layout;
-                }).setTextAlign(ColumnTextAlign.START).setFrozenToEnd(true).setFooter(btnConfirm);
+                });
         add(grid);
         gridRefresh(orderGoodRepository, userRepository);
 
